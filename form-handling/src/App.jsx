641f555c349cfdca84b2355982517ab0f29a655f
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-02-19 12:30:43 +0530

    Uploaded

---
+++ form-handling/src/App.jsx
@@ -0,0 +1,98 @@
+import { useForm } from 'react-hook-form'
+import toast, { Toaster } from 'react-hot-toast';
+
+
+function App() {
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({ mode: "onChange" })
+  const onSubmit = async (data) => {
+    // 1. Create a promise to simulate a database save
+    const loadToast = toast.loading('Submitting your data...');
+
+    try {
+      // Simulate a 2-second delay
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      // 2. Success toast
+      toast.success('Form submitted successfully!', {
+        id: loadToast, // This replaces the loading toast 
+        duration: 5000,
+        style: {
+          border: '1px solid #f97316',
+          padding: '12px',
+          color: '#c2410c',
+        },
+        iconTheme: {
+          primary: '#ea580c',
+          secondary: '#FFFAEE',
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      toast.error("Something went wrong!", { id: loadToast });
+    }
+  };
+
+  return (
+
+    <>
+      <div className="min-h-screen bg-white p-4 ">
+
+        <div className='bg-gradient-to-r from-red-600 via-orange-500 to-orange-400 text-white mt-10 p-4 max-w-5xl w-auto mx-4 mx-auto font-bold flex flex-col md:flex-row justify-between items-center rounded-xl shadow-sm'>
+
+          <div className='text-3xl md:ml-3'>Logo</div>
+
+
+          <div className='text-sm md:text-base md:mr-3 p-2 opacity-90 text-center md:text-right'>
+            This is a Form. Fill this up to feel how a modern form works.
+          </div>
+        </div>
+
+
+
+        {/* Input Form */}
+
+        <div className='bg-orange-50 border border-orange-100 mx-auto mt-16 p-10 rounded-2xl w-fit flex flex-col items-center shadow-none'>
+
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className='flex  items-center justify-between'>
+              <label className='font-semibold text-orange-900 w-24'>Name:</label>
+              <div className='flex-col flex'>
+                <input
+                  className='border-2 border-orange-200 flex-col rounded-lg p-2 outline-none focus:border-orange-500 transition-all'
+                  type="text"
+                  placeholder='Type Username'
+                  {...register("username", { required: "Username is required", minLength: { value: 3, message: "Username must be at least 3 characters" }, maxLength: { value: 10, message: "Username must be at most 10 characters" } })}
+                />
+                {errors.username && <span className='text-red-500 text-sm mt-1'>{errors.username.message}</span>}
+              </div>
+
+            </div>
+
+            <div className='flex items-center justify-between'>
+              <label className='font-semibold text-orange-900 w-24'>Password:</label>
+              <input
+                className='border-2 border-orange-200 rounded-lg p-2 outline-none focus:border-orange-500 transition-all'
+                type="password"
+                placeholder='Type Password'
+                {...register("password")}
+              />
+            </div>
+
+            <div className="flex justify-center pt-4">
+              <input
+                className='cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 enabled:active:scale-95 bg-gradient-to-r from-red-500 to-orange-500 text-white font-bold py-2 px-8 rounded-lg enabled:hover:scale-102 transition-transform shadow-md '
+                disabled={!isValid}
+                type="submit"
+                value="Submit"
+              />
+            </div>
+
+          </form>
+        </div>
+      </div>
+      <Toaster position='bottom-center'></Toaster>
+    </>
+  )
+}
+
+export default App
